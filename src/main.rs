89c7c1dfc0a0086ced2c19c497cd13e80b7cebbb
ino <87593133+inoUwU@users.noreply.github.com>
2023-11-22 16:40:34 +0000

    基本機能の実装

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 use dotenvy::dotenv;
+use once_cell::sync::Lazy;
+use regex::Regex;
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 use std::env;
 
+static DOMAIN_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"(x|twitter)\.com").unwrap());
+static URL_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"(https?://[^? ]+)(\?[^ ]+)?").unwrap());
+
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {why:?}");
+        if !msg.is_own(&ctx.cache) && DOMAIN_REGEX.is_match(&msg.content) {
+            let replaceed = DOMAIN_REGEX.replace_all(&msg.content, "fixupx.com");
+            if let Some(cap) = URL_REGEX.captures(&replaceed) {
+                if let Err(why) = msg.channel_id.say(&ctx.http, &cap[0]).await {
+                    println!("Error sending message: {why:?}");
+                }
             }
         }
     }
@@ -35,7 +43,6 @@ async fn main() {
         .event_handler(Handler)
         .await
         .expect("Err creating client");
-
     if let Err(why) = client.start().await {
         println!("Client error: {why:?}");
     }
